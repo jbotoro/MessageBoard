4bc822707f23ca27feed659790771fb47e2851ba
Jordan Black <jblack530@gmail.com>
2020-03-05 10:08:16 -0800

    added jquery rails, bootstrap, bootstrap scss and devise gems to project, added create and new actions for posts controller as well as post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,21 @@ class PostsController < ApplicationController
     def index
 
     end
+
+
+    def create
+        @post = Post.new(post_params)
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    ##private post params
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
+
 end
