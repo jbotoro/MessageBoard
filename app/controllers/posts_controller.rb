50629981b3e055a58f6e4d1b9ce995f12f067ca3
Jordan Black <jblack530@gmail.com>
2020-03-05 10:24:12 -0800

    added index and added redirect on successful save for create action for posts controller, added form partial using simple form for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
 
     def index
-
+        @posts = Post.all.order('created_at DESC')
     end
 
 
     def create
         @post = Post.new(post_params)
+        if @post.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
     end
 
     def new
