d6750830917d94c2ce4b41066188c22a2d19acf7
Jordan Black <jblack530@gmail.com>
2020-03-05 10:56:31 -0800

    added destroy, edit and update actions to posts controller, added edit and delete buttons to posts show page, editing a post works, added pop up warning to confirm deletion of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @posts = Post.all.order('created_at DESC')
     end
@@ -18,11 +20,34 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def show
+    end
+
+    def update
+        if @post.update(post_params)
+            redirect_to post_path
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to root_path
+    end
+
     ##private post params
 
     private
         def post_params
             params.require(:post).permit(:title, :body)
+        end
+
+        def find_post
+            @post = Post.find(params[:id])
         end
 
 end
